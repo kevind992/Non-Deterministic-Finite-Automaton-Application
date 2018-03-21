d933bf3c9785e6c236a077839b1a3a8b254d8e78
Kevin Delassus <g00270791@gmit.ie>
2018-03-21 21:02:32 +0000

    Added Thomsons Construction

--- shuntProblem.go
+++ shuntProblem.go
@@ -1,1 +1,100 @@
-package Non_Deterministic_Finite_Automaton_Application
+package main
+
+//import "fmt"
+
+type state struct {
+	symbol rune
+	edge1 *state
+	edge2 *state
+}
+
+type nfa struct {
+	initial *state
+	accept *state
+}
+
+func intoport(infix string) string{
+
+	specials := map[rune]int{'*': 10, '.':9,'|':8}
+
+	pofix, s := []rune{}, []rune{}
+
+	for _, r := range infix{
+		switch{
+
+		case r == '(':
+			s = append(s,r)
+		case r == ')':
+			for s[len(s)-1] != '('{
+				pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+			}
+			s = s[:len(s)-1]
+		case specials[r] > 0:
+			for len(s) > 0 && specials[r] <= specials[s[len(s)-1]]{
+				pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+			}
+			s = append(s,r)
+		default:
+			pofix = append(pofix, r)
+		}
+	}
+
+	for len(s) > 0{
+		pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+	}
+
+	return string(pofix)
+}
+
+func poregtonfa(pofix string) *nfa{
+
+	nfastack := []*nfa{}
+
+	for _, r := range pofix {
+		switch r {
+		case '.':
+			frag2 := nfastack[len(nfastack)-1]
+			nfastack = nfastack[:len(nfastack)-1]
+			frag1 := nfastack[len(nfastack)-1]
+			nfastack = nfastack[:len(nfastack)-1]
+
+			frag1.accept.edge1 = frag2.initial
+
+			nfastack = append(nfastack, &nfa{initial:frag1.initial, accept:frag2.accept})
+		case '|':
+			frag2 := nfastack[len(nfastack)-1]
+			nfastack = nfastack[:len(nfastack)-1]
+			frag1 := nfastack[len(nfastack)-1]
+			nfastack = nfastack[:len(nfastack)-1]
+
+			initial := state{edge1: frag1.initial, edge2: frag2.initial}
+			accept := state{}
+			frag1.accept.edge1 = &accept
+			frag2.accept.edge1 = &accept
+
+
+			nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})
+		case '*':
+			frag := nfastack[len(nfastack)-1]
+			nfastack = nfastack[:len(nfastack)-1]
+
+			accept := state{}
+			initial := state{edge1: frag.initial, edge2:&accept}
+			frag.accept.edge1 = frag.initial
+			frag.accept.edge2 = &accept
+
+			nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})
+		default:
+			accept := state{}
+			initial := state{symbol:r, edge1: &accept }
+			nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})
+		}
+
+	}
+
+	return nfastack[0]
+}
+
+func main()  {
+
+}
